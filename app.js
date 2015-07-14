697b231c888e477d21a381b97aa0d47076da8a7f
Jan Akerman <jakerman@scottlogic.com>
2015-07-14 16:49:37 +0100

    Indentified module now configured in RequireJs config

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+// Configure the path to our identified module.
+requirejs.config({
+    paths: {
+        identifiedModule: 'js/identifiedModule'
+    }
+});
+
 // Load in the main app file.
 requirejs(['js/main']);
--- js/identifiedModule.js
+++ js/identifiedModule.js
@@ -1,5 +1,5 @@
 // The ID name is optional - the path is used as the ID.
 // Here we're using it though.
- define('identifiedModule', ['./print'], function(print) {
+ define('identifiedModule', ['js/print'], function(print) {
     return function() { print('I\'m loaded by identifier!'); };
 });
